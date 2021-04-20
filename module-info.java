794956b51ece949e06dba9802e8897a4dfa5d642
Lihes cheriet <66468238+lihescheriet@users.noreply.github.com>
2021-04-20 10:25:42 +0200

    Initial commit

---
+++ module-info.java
@@ -0,0 +1,3 @@
+module cours_Java {
+	
+}
---
+++ visite.java
@@ -0,0 +1,14 @@
+
+public class visite {
+	String nom_etudiant = "cheriet";
+	String date = "21 janvier 2015";
+	int note = 12;
+	visite()
+	{
+		
+	}
+	visite(int note, String date, String nom_etudiant)
+	{
+		
+	}
+}
